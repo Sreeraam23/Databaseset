b3c15574e379bd9f57f4440ac98b188ab8662abd
Sreeraam23 <sreeraam2304@gmail.com>
2023-03-10 20:44:27 +0530

    Databaseset

---
+++ src/DBtest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class DBtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
